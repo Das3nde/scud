aa2bf0c79086f9a0af6bba8cfdd549e974694c68
Das3nde <jknutson@alumni.princeton.edu>
2015-08-31 10:38:12 -0700

    Early modal stuff in place

--- src/home/HomeCtrl.js
+++ src/home/HomeCtrl.js
@@ -3,8 +3,13 @@
 // @ngInject
 module.exports = function ($scope, $modal) {
   this.joinStable = () => {
-    $modal.open({
+    let modalInstance = $modal.open({
       template: require('./templates/join_stable_modal.jade')
+    })
+
+    modalInstance.result.then(function (selection) {
+    }, function () {
+      console.log('Modal dismissed at: ' + new Date())
     })
   }
 }
